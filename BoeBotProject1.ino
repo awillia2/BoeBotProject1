f847af2cce10a6cd105a2497f936f0e2c4727cd8
willip27 <pariswilliams@Pariss-MacBook-Pro.local>
2016-10-17 23:33:30 -0400

    Changes to code

--- BoeBotProject1.ino
+++ BoeBotProject1.ino
@@ -1,16 +1,15 @@
 #include <QTRSensors.h>
 #include <Servo.h>
+  Servo servoRight; // Define right servo
+  Servo servoLeft;  // Define left servo
 #define NUM_SENSORS   3     // number of sensors used
 #define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
-#define EMITTER_PIN   2     // emitter is controlled by digital pin 2
 // sensors 1 through 3 are connected to digital pins 2 through 4, respectively
-QTRSensorsRC qtrrc((unsigned char[]) {2,3,4}, NUM_SENSORS, TIMEOUT, EMITTER_PIN); 
+QTRSensorsRC qtrrc((unsigned char[]) {2,3,4}, NUM_SENSORS, TIMEOUT); 
 unsigned int sensorValues[NUM_SENSORS];
 
 void setup() {
 //  Declare instance of Servo and then init to the proper pins
-  Servo servoRight;
-  Servo servoLeft;
   servoRight.attach(13);
   servoLeft.attach(12);
   
@@ -41,29 +40,27 @@ void loop() {
   servoRight.writeMicroseconds(1600);//Always Above 1500. 1515
   
   for(unsigned char i = 0; i < NUM_SENSORS; i++) {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
+    Serial.println(sensorValues[i]);
   }
   
-  if(sensorValues[0] < 400) {
-    Serial.print("\nSensor 0: ");
-    Serial.print(sensorValues[0]);
-    servoLeft.writeMicroseconds(1500);//Always Under 1500. This is turning left.
-    servoRight.writeMicroseconds(1600);//Always Above 1500-this changes
-  }
-      
-  if(sensorValues[2] < 400) {
-    Serial.print("\nSensor 2: ");
-    Serial.print(sensorValues[2]);
-    servoLeft.writeMicroseconds(1400);//Always Under 1500-this changes 
-    servoRight.writeMicroseconds(1500);//Always Above 1500. This is turning right.
+  if(sensorValues[0] > 400) {
+    Serial.println("Sensor 0: " + sensorValues[0]);
+    turn(1500,2000, 1000);  // left turn
   }
 
-  Serial.println(); // uncomment this line if you are using raw values
-  //Serial.println(position); // comment this line out if you are using raw values
+  turn(1400, 1600, 0); // Moving forward
+     
+  if(sensorValues[2] > 400) {
+    Serial.println("Sensor 2: " + sensorValues[2]);
+    turn(1000, 1500, 1000); // right turn
+  }
   
-  delay(1000);
-  
+  delay(1000);  
+
 }
 
-
+void turn(int left, int right, int delayTime) {
+  servoLeft.writeMicroseconds(left);
+  servoRight.writeMicroseconds(right);
+  delay(delayTime);
+}
